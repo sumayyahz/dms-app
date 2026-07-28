bda6d66fdd37887f10a58dc424265aa3a25bb2eb
sumayyahz <sumayyah16896@gmail.com>
2026-07-28 10:03:05 +0700

    Add entities index file for exports

---
+++ backend/src/domain/entities/index.ts
@@ -0,0 +1,8 @@
+export { User, UserRole } from './User.entity';
+export { Document, DocumentStatus } from './Document.entity';
+export { DocumentVersion } from './DocumentVersion.entity';
+export { DocumentMetadata } from './DocumentMetadata.entity';
+export { DocumentContent } from './DocumentContent.entity';
+export { Category } from './Category.entity';
+export { Tag } from './Tag.entity';
+export { AuditLog, AuditAction } from './AuditLog.entity';
